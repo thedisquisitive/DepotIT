2f9e770b26141d7e7139ecabb55f5a3cfff1ade6
thedisquisitive <zach@thedisquisitive.com>
2024-04-28 16:25:11 -0400

    fixed encoding error... I hope

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Bah"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
