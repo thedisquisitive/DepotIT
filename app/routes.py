024af28e2a8d8b721d6ca57b27364e17fc566b31
thedisquisitive <zach@thedisquisitive.com>
2024-04-28 22:26:09 -0400

    started login boilerplate

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,14 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
---
+++ config.py
@@ -0,0 +1,5 @@
+﻿import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'acs_secret_key'
+
